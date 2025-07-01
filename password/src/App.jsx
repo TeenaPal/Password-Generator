e8f0964375339ac10f40816a2d03d1d75b2a8989
TeenaPal <pteena495@gmail.com>
2025-07-01 18:14:21 +0530

    here applying the event on cheeckbox

--- password/src/App.jsx
+++ password/src/App.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
+ let [uppercase,setuppercase] = useState(false);
+  let [lowercase,setlowercase] = useState(false);
+   let [num,setnum] = useState(false);
+    let [symbol,setsymbol] = useState(false);
   return (
     <div className="container-fluid">
  <div className="elem_container">
@@ -21,22 +25,22 @@ function App() {
   
   <div className="pass_length">
 <label>Include uppercase letters</label>
-<input type="checkbox" className='check' />
+<input type="checkbox" className='check' checked ={uppercase} onChange={()=>setuppercase(!uppercase)} />
   </div>
 
   <div className="pass_length">
 <label>Include lowercase letters</label>
-<input type="checkbox" className='check'   />
+<input type="checkbox" className='check' checked ={lowercase} onChange={()=>setlowercase(!lowercase)}    />
   </div>
 
   <div className="pass_length">
 <label>Include Symbol</label>
-<input type="checkbox" className='check'  />
+<input type="checkbox" className='check' checked ={symbol} onChange={()=>setsymbol(!symbol)}  />
   </div>
 
   <div className="pass_length">
 <label>Include numbers</label>
-<input type="Checkbox" className='check'  />
+<input type="Checkbox" className='check' checked ={num} onChange={()=>setnum(!num)} />
   </div>
 
  <div className="generate-button">
---
+++ password/src/data/password_value.jsx
@@ -0,0 +1,4 @@
+export let symbol = "!@#$%^&*()>?/";
+export let UP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export let SC = "abcdefghijklmnopqrstuvwxyz";
+export let num = "0123456789";
